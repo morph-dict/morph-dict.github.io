9f7939e4259e323e9915c7d0ac98db5b381bea39
Nikolai Mavrenkov <koluch@koluch.ru>
2015-11-01 22:06:52 +0300

    Use Underscore; implement fetching paradigms

---
+++ main.js
@@ -0,0 +1,145 @@
+/**
+ * --------------------------------------------------------------------
+ * Copyright 2015 Nikolay Mavrenkov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * --------------------------------------------------------------------
+ *
+ * Author:  Nikolay Mavrenkov <koluch@koluch.ru>
+ * Created: 01.11.2015 23:04
+ */
+
+function jqPromise(def) {
+    return new Promise(function(done, fail){
+        def.done(done).fail(fail);
+    })
+}
+
+function indexResolver(perFile) {
+    return function(index) {
+        return {
+            base: Math.floor(index / perFile),
+            offset: index % perFile
+        }
+    }
+}
+
+function search(toSearch) {
+    var dataRoot = "data";
+
+
+    return jqPromise($.ajax(dataRoot+"/index.json")).then(function(indexFile){
+        var index = indexResolver(indexFile.itemsPerFile);
+        var treeDir = dataRoot + "/" + indexFile.treeDir;
+        var dictLexDir = dataRoot + "/" + indexFile.dictLexDir;
+        var dictParadigmDir = dataRoot + "/" + indexFile.dictParadigmDir;
+
+        return new Promise(function (resolve, fail) {
+            function search(nextNodeIndex, rest) {
+
+                nextNodeIndex = index(nextNodeIndex);
+
+                var file = treeDir + "/" + nextNodeIndex.base + ".json";
+                jqPromise($.ajax(file)).then(function (data) {
+                    var next = data[nextNodeIndex.offset];
+                    if (rest.length == 0) {
+                        if (next[1]) {
+                            resolve(next[1]);
+                        }
+                        else {
+                            fail({status: "not_found", data: toSearch});
+                        }
+                    }
+                    else {
+                        var branches = next[0];
+                        var branchIndex = branches[rest.charAt(0)];
+                        if (branchIndex) {
+                            search(branchIndex, rest.substr(1));
+                        }
+                        else {
+                            fail({status: "not_found", data: toSearch});
+                        }
+                    }
+                }).catch(function (xhr, e) {
+                    fail(e);
+                })
+            }
+            search(indexFile.root, toSearch);
+        })
+
+            // Make it looks better
+            .then(function(data){
+                return data.map(function(x){
+                    return {
+                        ancode: x[0],
+                        lexemeRec: x[1]
+                    }
+                });
+            })
+
+            // Fetch lexeme rec
+            .then(function(data){
+                return Promise.all(data.map(function(node){
+                    var idx = index(node.lexemeRec);
+                    var fileName = dictLexDir + "/"  + idx.base + ".json";
+                    return jqPromise($.ajax(fileName)).then(function(lexemeRecFile){
+                        var lexemeRec = lexemeRecFile[idx.offset];
+                        lexemeRec = {
+                            basis: lexemeRec[0],
+                            paradigmNum: lexemeRec[1],
+                            accentParadigmNum: lexemeRec[2],
+                            userSessionNum: lexemeRec[3],
+                            ancode: lexemeRec[4],
+                            prefixParadigmNum: lexemeRec[5]
+                        };
+
+                        return _.extend(node, {
+                            lexemeRec: lexemeRec
+                        })
+                    })
+                }))
+            })
+
+            // Fetch paradigms
+            .then(function(data){
+                return Promise.all(data.map(function(node){
+                    var idx = index(node.lexemeRec.paradigmNum);
+                    var fileName = dictLexDir + "/"  + idx.base + ".json";
+                    return jqPromise($.ajax(fileName)).then(function(paradigmRulesFile){
+                        return _.extend(node, {
+                            paradigmRules: paradigmRulesFile[idx.offset]
+                        })
+                    })
+                }))
+            })
+
+            .then(function(data){
+                console.log(data);
+                return data;
+            })
+
+    });
+}
+
+$(function () {
+    $("#search").submit(function(e){
+        var toSearch = $("#word").val();
+        $("#result").text("searching...");
+        search(toSearch).then(function(result){
+            $("#result").text(JSON.stringify(result));
+        }).catch(function(e){
+            throw e;
+        });
+        e.preventDefault();
+    });
+})
